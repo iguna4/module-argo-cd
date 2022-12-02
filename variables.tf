5f2b2cf485655b35fc68b94617791d06a9e6e25b
iguna4 <evaristeiguna4@gmail.com>
2022-12-02 10:35:07 +0200

    ArgoCD module init

---
+++ variables.tf
@@ -0,0 +1,30 @@
+
+variable "kubernetes_cluster_id" {
+  type = string
+  #   default     = ""
+  #   description = "description"
+}
+
+variable "kubernetes_cluster_cert_data" {
+  type = string
+  #   default     = ""
+  #   description = "description"
+}
+
+variable "kubernetes_cluster_endpoint" {
+  type = string
+  #   default     = ""
+  #   description = "description"
+}
+
+variable "kubernetes_cluster_name" {
+  type = string
+  #   default     = ""
+  #   description = "description"
+}
+
+variable "eks_nodegroup_id" {
+  type = string
+  #   default     = ""
+  #   description = "description"
+}
